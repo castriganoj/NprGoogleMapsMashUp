4cc9f43db1c7078f18b99e2d973cf370eccb0efa
Jim C <castriganoj@gmail.com>
2015-09-13 02:17:37 +0000

    completed 1st requirement. returned story objects and displyed title, date, ist sentence of article and link on html page.

--- js/pull_stories.js
+++ js/pull_stories.js
@@ -1,18 +1,37 @@
 
 $( document ).ready(function() {
     
-   // console.log( "ready!" );
-
-var nprUrl = "https://api.npr.org/query?fields=title,storyDate,titles&dateType=story&output=JSON&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
-$.getJSON( nprUrl, function( data ) {
-  var items = [];
-  $.each( data.list.title.$text, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  var nprUrl = "https://api.npr.org/query?fields=title,storyDate,text,listText&dateType=story&output=JSON&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+  
+  $.getJSON( nprUrl, function( data ) {
+    
+    var items = [];
+    $.each( data.list.story, function( key, val ) {
+      items.push( 
+        "<li id='" + key + "'>"
+        + "<ul>" +
+            "<li> Title: "+ 
+              val.title.$text +
+            "</li>" +
+            "<li> Text: "+
+              val.text.paragraph[0].$text +
+            "</li>" +
+            "<li> Link: "+
+              val.link[0].$text +
+            "</li>" +
+            "<li> Date: "+
+              val.storyDate.$text +
+            "</li>" +
+          "</ul>" +
+        "</li>" +
+      "");
+    });
+   
+    $( "<ol/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "#StoriesArea" );
+    
   });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "#StoriesArea" );
-});
+  
 });
