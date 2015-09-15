1bc60cff692d5eb076c506ff5a05179488532494
Jim C <castriganoj@gmail.com>
2015-09-15 19:49:35 +0000

    Add test json file and get json method

---
+++ js/NPR_Output.js
@@ -0,0 +1,256 @@
+{
+    "version": "0.94",
+    "list": {
+        "title": {
+            "$text": "NPR Topics: U.S."
+        },
+        "teaser": {
+            "$text": "NPR coverage of national news, U.S. politics, elections, business, arts, culture, health and science, and technology. Subscribe to the NPR Nation RSS feed."
+        },
+        "miniTeaser": {},
+        "link": [{
+            "type": "api",
+            "$text": "http://api.npr.org/query?id=1003&meta=inherit&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+        }, {
+            "type": "html",
+            "$text": "http://www.npr.org/sections/us/?ft=nprml&f=1003"
+        }],
+        "story": [{
+            "id": "440363919",
+            "link": [{
+                "type": "html",
+                "$text": "http://www.npr.org/sections/thetwo-way/2015/09/14/440363919/dancing-baby-wins-copyright-case?ft=nprml&f=1003"
+            }, {
+                "type": "api",
+                "$text": "http://api.npr.org/query?id=440363919&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+            }, {
+                "type": "short",
+                "$text": "http://n.pr/1KaODK7"
+            }],
+            "text": {
+                "paragraph": [{
+                    "num": "1",
+                    "$text": "A baby bobs up and down in a kitchen, as a Prince song plays in the background. His mother laughs in the background and his older sister zooms in and out of the frame."
+                }, {
+                    "num": "2",
+                    "$text": "This innocuous 29-second home video clip was posted to YouTube in 2007 and sparked a long legal proceeding on copyright and fair use law."
+                }, {
+                    "num": "3",
+                    "$text": "In the case, Lenz v. Universal — which has gained notoriety as the \"dancing baby\" lawsuit — Universal Music Group sent YouTube a warning to take the video down, claiming copyright infringement under the Digital Millennium Copyright Act. Then, Stephanie Lenz, poster of the video and mother of the baby, represented by Electronic Frontier Foundation, sued Universal for wrongly targeting lawful fair use."
+                }, {
+                    "num": "4",
+                    "$text": "Today, eight years later, a federal appeals court has sided with the dancing baby."
+                }, {
+                    "num": "5",
+                    "$text": "The three-judge panel in San Francisco ruled unanimously in favor of Lenz and the EFF, saying copyright holders must consider whether a use of material is fair before sending a take-down notice."
+                }, {
+                    "num": "6",
+                    "$text": "NPR's Laura Sydell reports for our Newscasts:"
+                }, {
+                    "num": "7"
+                }, {
+                    "num": "8",
+                    "$text": "\"Fair Use permits people to use copyrighted material in certain situations like satire or news. Now three judges on the 9th circuit say that unless Universal evaluated whether it was fair use, it may have violated the rights of the video maker. The case is significant because critics say copyright owners like Universal abuse the take down process often at the expense of free expression.\""
+                }, {
+                    "num": "9"
+                }, {
+                    "num": "10",
+                    "$text": "The timing of the case is also significant, the EFF said in a statement."
+                }, {
+                    "num": "11"
+                }, {
+                    "num": "12",
+                    "$text": "\"Heated political campaigns—like the current presidential primaries—have historically led to a rash of copyright takedown abuse. Criticism of politicians often includes short clips of campaign appearances in order to make arguments to viewers, and broadcast networks, candidates, and other copyright holders have sometimes misused copyright law in order to remove the criticism from the Internet."
+                }, {
+                    "num": "13",
+                    "$text": "\"'The decision made by the appeals court today has ramifications far beyond Ms. Lenz's rights to share her video with family and friends,' said McSherry. 'We will all watch a lot of online video and analysis of presidential candidates in the months to come, and this ruling will help make sure that information remains uncensored.'\""
+                }, {
+                    "num": "14",
+                    "$text": " [Copyright 2015 NPR]"
+                }]
+            }
+        }, {
+            "id": "440337277",
+            "link": [{
+                "type": "html",
+                "$text": "http://www.npr.org/sections/thetwo-way/2015/09/14/440337277/bail-denied-for-former-police-officer-who-shot-and-killed-unarmed-black-man?ft=nprml&f=1003"
+            }, {
+                "type": "api",
+                "$text": "http://api.npr.org/query?id=440337277&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+            }, {
+                "type": "short",
+                "$text": "http://n.pr/1QAZq4O"
+            }],
+            "text": {
+                "paragraph": [{
+                    "num": "1",
+                    "$text": "On Monday, a judge in Charleston, S.C., denied bail to a white ex-police officer, Michael Slager, who is charged with murder in the April shooting death of an unarmed black man, Walter Scott."
+                }, {
+                    "num": "2",
+                    "$text": "Slager, 33, shot Scott, 50, in the back eight times as Scott ran away during a traffic stop. The shooting was filmed by a bystander."
+                }, {
+                    "num": "3",
+                    "$text": "Circuit Judge Clifton Newman said in his decision that releasing Slager would \"constitute an unreasonable danger to the community.\""
+                }, {
+                    "num": "4",
+                    "$text": "The Associated Press reports:"
+                }, {
+                    "num": "5"
+                }, {
+                    "num": "6",
+                    "$text": "\"Prosecutor Scarlett Wilson on Thursday had called Slager 'a firing squad and executioner' and said he planted evidence, taking his Taser from where it fell and dropping it near Scott's body hundreds of feet away."
+                }, {
+                    "num": "7",
+                    "$text": "\"Slager's attorneys argued that their client posed no threat to the public, filing 150 pages of documents ahead of the hearing. The documents included a toxicology report showing there was cocaine in Scott's blood when he was killed, as well as a psychological assessment that Slager poses little future danger of committing violence.\""
+                }, {
+                    "num": "8",
+                    "$text": " [Copyright 2015 NPR]"
+                }]
+            }
+        }, {
+            "id": "440327634",
+            "link": [{
+                "type": "html",
+                "$text": "http://www.npr.org/2015/09/14/440327634/presidential-candidates-use-social-media-to-repackage-campaign-moments?ft=nprml&f=1003"
+            }, {
+                "type": "api",
+                "$text": "http://api.npr.org/query?id=440327634&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+            }, {
+                "type": "short",
+                "$text": "http://n.pr/1gmVyY1"
+            }],
+            "text": {
+                "paragraph": [{
+                    "num": "1",
+                    "$text": "[Copyright 2015 NPR]"
+                }]
+            }
+        }, {
+            "id": "440327613",
+            "link": [{
+                "type": "html",
+                "$text": "http://www.npr.org/2015/09/14/440327613/rough-fire-spreads-near-ancient-sequoia-grove-in-california?ft=nprml&f=1003"
+            }, {
+                "type": "api",
+                "$text": "http://api.npr.org/query?id=440327613&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+            }, {
+                "type": "short",
+                "$text": "http://n.pr/1gmVyHF"
+            }],
+            "text": {
+                "paragraph": [{
+                    "num": "1",
+                    "$text": "[Copyright 2015 NPR]"
+                }]
+            }
+        }, {
+            "id": "440327606",
+            "link": [{
+                "type": "html",
+                "$text": "http://www.npr.org/2015/09/14/440327606/valley-fire-in-northern-california-devours-homes-forces-evacuations?ft=nprml&f=1003"
+            }, {
+                "type": "api",
+                "$text": "http://api.npr.org/query?id=440327606&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+            }, {
+                "type": "short",
+                "$text": "http://n.pr/1gmVyHB"
+            }],
+            "text": {
+                "paragraph": [{
+                    "num": "1",
+                    "$text": "[Copyright 2015 NPR]"
+                }]
+            }
+        }, {
+            "id": "440327655",
+            "link": [{
+                "type": "html",
+                "$text": "http://www.npr.org/2015/09/14/440327655/illinois-lottery-suspends-payouts-until-state-lawmakers-settle-budget-stalemate?ft=nprml&f=1003"
+            }, {
+                "type": "api",
+                "$text": "http://api.npr.org/query?id=440327655&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+            }, {
+                "type": "short",
+                "$text": "http://n.pr/1gmVyY9"
+            }],
+            "text": {
+                "paragraph": [{
+                    "num": "1",
+                    "$text": "[Copyright 2015 NPR]"
+                }]
+            }
+        }, {
+            "id": "440327648",
+            "link": [{
+                "type": "html",
+                "$text": "http://www.npr.org/2015/09/14/440327648/harvards-hasty-pudding-club-holds-1st-auditions-for-female-performers?ft=nprml&f=1003"
+            }, {
+                "type": "api",
+                "$text": "http://api.npr.org/query?id=440327648&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+            }, {
+                "type": "short",
+                "$text": "http://n.pr/1gmVyY7"
+            }],
+            "text": {
+                "paragraph": [{
+                    "num": "1",
+                    "$text": "[Copyright 2015 NPR]"
+                }]
+            }
+        }, {
+            "id": "440327627",
+            "link": [{
+                "type": "html",
+                "$text": "http://www.npr.org/2015/09/14/440327627/ferguson-commission-report-highlights-racial-inequality-in-st-louis-region?ft=nprml&f=1003"
+            }, {
+                "type": "api",
+                "$text": "http://api.npr.org/query?id=440327627&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+            }, {
+                "type": "short",
+                "$text": "http://n.pr/1gmVx6k"
+            }],
+            "text": {
+                "paragraph": [{
+                    "num": "1",
+                    "$text": "[Copyright 2015 NPR]"
+                }]
+            }
+        }, {
+            "id": "440327620",
+            "link": [{
+                "type": "html",
+                "$text": "http://www.npr.org/2015/09/14/440327620/russia-sends-armaments-to-syrian-military-base?ft=nprml&f=1003"
+            }, {
+                "type": "api",
+                "$text": "http://api.npr.org/query?id=440327620&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+            }, {
+                "type": "short",
+                "$text": "http://n.pr/1gmVyXV"
+            }],
+            "text": {
+                "paragraph": [{
+                    "num": "1",
+                    "$text": "[Copyright 2015 NPR]"
+                }]
+            }
+        }, {
+            "id": "440327592",
+            "link": [{
+                "type": "html",
+                "$text": "http://www.npr.org/2015/09/14/440327592/bernie-sanders-speaks-to-christian-conservatives-at-liberty-university?ft=nprml&f=1003"
+            }, {
+                "type": "api",
+                "$text": "http://api.npr.org/query?id=440327592&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+            }, {
+                "type": "short",
+                "$text": "http://n.pr/1gmVyHt"
+            }],
+            "text": {
+                "paragraph": [{
+                    "num": "1",
+                    "$text": "[Copyright 2015 NPR]"
+                }]
+            }
+        }]
+    }
+}
--- js/pull_stories.js
+++ js/pull_stories.js
@@ -1,37 +1,41 @@
+$(document).ready(function() {
 
-$( document ).ready(function() {
-    
-  var nprUrl = "https://api.npr.org/query?fields=title,storyDate,text,listText&dateType=story&output=JSON&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
-  
-  $.getJSON( nprUrl, function( data ) {
-    
-    var items = [];
-    $.each( data.list.story, function( key, val ) {
-      items.push( 
-        "<li id='" + key + "'>"
-        + "<ul>" +
-            "<li> Title: "+ 
-              val.title.$text +
-            "</li>" +
-            "<li> Text: "+
-              val.text.paragraph[0].$text +
-            "</li>" +
-            "<li> Link: "+
-              val.link[0].$text +
-            "</li>" +
-            "<li> Date: "+
-              val.storyDate.$text +
-            "</li>" +
-          "</ul>" +
-        "</li>" +
+  /*var nprUrl = "https://api.npr.org/query?fields=title,storyDate,text,listText&dateType=story&output=JSON&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
+
+  //$.getJSON( nprUrl, function( data ) {
+
+  var items = [];
+  $.each(data.list.story, function(key, val) {
+    items.push(
+      "<li id='" + key + "'>" + "<ul>" +
+      "<li> Title: " +
+      val.title.$text +
+      "</li>" +
+      "<li> Text: " +
+      val.text.paragraph[0].$text +
+      "</li>" +
+      "<li> Link: " +
+      val.link[0].$text +
+      "</li>" +
+      "<li> Date: " +
+      val.storyDate.$text +
+      "</li>" +
+      "</ul>" +
+      "</li>" +
       "");
-    });
-   
-    $( "<ol/>", {
-      "class": "my-new-list",
-      html: items.join( "" )
-    }).appendTo( "#StoriesArea" );
-    
   });
-  
+
+  $("<ol/>", {
+    "class": "my-new-list",
+    html: items.join("")
+  }).appendTo("#StoriesArea");
+
+});
+*/
+
+//loop thru all text in each story and categorize with a city name
+ $.getJSON("js/NPR_Output.js", function(json) {
+    console.log(json); // this will show the info it in firebug console
+  });
+
 });
