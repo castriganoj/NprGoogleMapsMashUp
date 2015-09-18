02422df6b0e78158177d738b1dc7622f46140c89
Jim C <castriganoj@gmail.com>
2015-09-18 19:55:56 +0000

    created loops for extracting text, and title from npr json. create object construtor for new array of npr objects packaged for geocode check and marker

--- js/pull_stories.js
+++ js/pull_stories.js
@@ -1,5 +1,3 @@
-$(document).ready(function() {
-
   /*var nprUrl = "https://api.npr.org/query?fields=title,storyDate,text,listText&dateType=story&output=JSON&apiKey=MDE4OTM0NDk1MDE0Mjk3MDg1MDFhYjFiMg001"
 
   //$.getJSON( nprUrl, function( data ) {
@@ -32,10 +30,66 @@ $(document).ready(function() {
 
 });
 */
+ 
+ $(document).ready(function() {
+   console.log('hey');
+   $(JsonGetter.pullFile)
+  
+ });
 
-//loop thru all text in each story and categorize with a city name
- $.getJSON("js/NPR_Output.js", function(json) {
-    console.log(json); // this will show the info it in firebug console
-  });
+function storyObj(title,text, cityName) {
+        this.title = title;
+        this.text = text;
+        this.cityName = cityName;
+    }
 
-});
+
+ var JsonGetter = {
+     
+   //get json file
+   pullFile: function() {
+     $.getJSON("js/NPR_Output.json", function(json) {
+       var data = json;
+       JsonGetter.getData(data);
+     });
+   }
+
+   ,getData: function(d) {
+     var Npr_json = d;
+     JsonGetter.pullText(Npr_json)
+   }
+
+   ,pullText: function(Npr_json) {
+       var storyObjs;
+       var storyTitle;
+       var stories = []
+       var storyTxt;
+
+        for (var i = 0; i < Npr_json.list.story.length; i++) {
+            storyObjs = Npr_json.list.story[i].text.paragraph;
+            storyTitle = Npr_json.list.story[i].title.$text;
+            storyTxt = "";
+            
+ 
+            $.each(storyObjs, function( index, value ) {
+                storyTxt += value.$text;
+            });
+            
+                                                            // create array of objects with title story and city name properties.
+            stories.push(new storyObj(storyTitle, storyTxt,""));
+            
+        }
+     // will like need to loop inside loop
+   }
+ }
+ 
+  
+// set up story object for check and delivery
+//need object constrictor
+
+
+//pass each word in text to geocode until cityNameis found
+
+//pass cityName back to storyObj along with long and lat for placement on map
+
+// add marker to map with long and lat, at text to marker with city name and story title. 
